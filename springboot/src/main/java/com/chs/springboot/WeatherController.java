9f866b3da79b2e91256b4801d51fc690509ccead
honeychest <scollo1982@gmail.com>
2026-02-06 20:47:20 +0900

    모바일용 UI를 적용

--- springboot/src/main/java/com/chs/springboot/WeatherController.java
+++ springboot/src/main/java/com/chs/springboot/WeatherController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
